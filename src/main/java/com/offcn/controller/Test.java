7f4402cf6387c617c36d50f13cdce6688784ce03
TEL <1198563938@qq.com>
2020-08-06 20:33:38 +0800

    测试提交2

---
+++ src/main/java/com/offcn/controller/Test.java
@@ -0,0 +1,15 @@
+package com.offcn.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @RequestMapping("/hello")
+    public String test(){
+        System.out.println("哈哈");
+        return "111";
+    }
+}
