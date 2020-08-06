dd0b82e1a77471bb917a0233d21275950498f697
TEL <1198563938@qq.com>
2020-08-06 20:29:07 +0800

    测试提交1

---
+++ src/test/java/com/offcn/GitTestApplicationTests.java
@@ -0,0 +1,14 @@
+package com.offcn;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
