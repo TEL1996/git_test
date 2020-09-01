591dbfc6e9e67814e816315ae6de82433efc202a
TEL <1198563938@qq.com>
2020-09-01 17:51:01 +0800

    测试提交4

--- src/main/java/com/offcn/bean/Car.java
+++ src/main/java/com/offcn/bean/Car.java
@@ -1,4 +1,22 @@
 package com.offcn.bean;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data //get set
+@AllArgsConstructor //有参
+@NoArgsConstructor  //无参
 public class Car {
+
+    private Integer id;
+    private String name;
+    private Float price;
+   @JsonFormat(pattern="yyyy-MM-dd HH:mm:ss",timezone="GMT+8")
+    private Date createdate;
+
+
 }
--- src/main/java/com/offcn/controller/Test.java
+++ src/main/java/com/offcn/controller/Test.java
@@ -1,15 +1,20 @@
 package com.offcn.controller;
 
+import com.offcn.bean.Car;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
 
 @RestController
 @RequestMapping("/test")
 public class Test {
 
     @RequestMapping("/hello")
-    public String test(){
+    public Car test(){
         System.out.println("哈哈");
-        return "1222";
+        Car car = new Car(1,"笑话",199.1f,new Date());
+        return car;
     }
 }
